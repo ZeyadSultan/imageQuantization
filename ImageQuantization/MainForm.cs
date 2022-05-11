dad3147ff4449e5bb7046c202bbe3d513fa7e4fb
Zeyad Khaled Sultan <89984601+ZeyadSultan@users.noreply.github.com>
2022-05-12 01:16:08 +0200

    distinctColors&createGraph

--- ImageQuantization/MainForm.cs
+++ ImageQuantization/MainForm.cs
@@ -8,8 +8,13 @@ using System.Windows.Forms;
 
 namespace ImageQuantization
 {
+    
     public partial class MainForm : Form
     {
+        public static int distinct_colors = 0;
+        public static RGBPixel[] distColors = new RGBPixel[100000];
+        public static double[,] edgeWeight = new double[distinct_colors, distinct_colors];
+        public static List<KeyValuePair<int, double>>[] vertix = new List<KeyValuePair<int, double>>[distinct_colors];
         public MainForm()
         {
             InitializeComponent();
@@ -35,11 +40,57 @@ namespace ImageQuantization
         {
             double sigma = double.Parse(txtGaussSigma.Text);
             int maskSize = (int)nudMaskSize.Value ;
+            
             ImageMatrix = ImageOperations.GaussianFilter1D(ImageMatrix, maskSize, sigma);
             ImageOperations.DisplayImage(ImageMatrix, pictureBox2);
         }
 
-       
-       
+        public static void findDistinctColors(RGBPixel[,] imagePixels)
+        {
+            bool[,,] visited = new bool[255,255,255];
+            for(int i = 0;i<imagePixels.GetLength(0);i++)
+            {
+                for(int j=0;j<imagePixels.GetLength(1);j++)
+                {
+                    visited[imagePixels[i, j].red, imagePixels[i, j].green, imagePixels[i, j].blue] = true;
+                }
+            }
+
+            for (int r=0;r<256; r++)
+            {
+                for(int g=0;g<256; g++)
+                {
+                    for(int  b=0;b<256;b++)
+                    {
+                        if(visited[r,g,b])
+                        {
+                            RGBPixel pixel;
+                            pixel.red = (byte)r;
+                            pixel.green = (byte)g;
+                            pixel.blue = (byte)b;
+
+                            distColors[distinct_colors] = pixel;
+                            distinct_colors++;
+                        }
+                    }
+                }
+            }
+        }
+        public static void createGraph()
+        {
+            for(int i =0;i<distinct_colors;i++)
+            {
+                for(int j=0;j<distinct_colors;j++)
+                {
+                    if(i != j)
+                    {
+                        edgeWeight[i, j] = Math.Sqrt(Math.Pow((distColors[i].red) - (distColors[j].red), 2) +
+                            Math.Pow((distColors[i].blue) - (distColors[j].blue), 2) +
+                            Math.Pow((distColors[i].green) - (distColors[j].green), 2));
+                        vertix[i].Add(new KeyValuePair<int, double>(j, edgeWeight[i, j]));
+                    }
+                }
+            }
+        }
     }
 }
